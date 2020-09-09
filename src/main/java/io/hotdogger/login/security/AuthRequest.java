1cffcb71badda353860da8f7498b7e22a2bf3305
Stuart Marsh <stujmar@gmail.com>
2020-09-08 21:21:15 -0500

    added AuthRequest class

---
+++ src/main/java/io/hotdogger/login/security/AuthRequest.java
@@ -0,0 +1,35 @@
+package io.hotdogger.login.security;
+
+/**
+ * AuthRequest class is use as a request body to send in a POST request (the credentials) when
+ * logging into the API.
+ */
+public class AuthRequest {
+
+    private String email;
+    private String password;
+
+    public AuthRequest() {
+    }
+
+    public AuthRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
