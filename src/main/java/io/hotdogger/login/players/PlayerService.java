0e123695870bc87cb22f10dfae0f60e60b8e0325
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-09-28 22:39:08 -0500

    populate the PlayerService interface

--- src/main/java/io/hotdogger/login/players/PlayerService.java
+++ src/main/java/io/hotdogger/login/players/PlayerService.java
@@ -1,4 +1,16 @@
 package io.hotdogger.login.players;
 
+import java.util.List;
+
 public interface PlayerService {
+
+    Player createPlayer(Player newPlayer);
+
+    List<Player> getAllPlayers();
+
+    Player getPlayerById(Long playerId);
+
+    Player updatePlayerById(Long playerId, Player updatedPlayer);
+
+    void deletePlayerById(Long playerId);
 }
