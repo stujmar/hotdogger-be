df0fc28a740e3cb5e51a8017539d31d8b11a4f6c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-01 17:31:36 -0500

    refactored the PlayerRepo from being the CustomersDao

--- src/main/java/io/hotdogger/login/players/PlayerRepo.java
+++ src/main/java/io/hotdogger/login/players/PlayerRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomersDao extends JpaRepository<Customer, Long> {
-    Customer findByEmail(String email);
+public interface PlayerRepo extends JpaRepository<Player, Long> {
+    Player findByEmail(String email);
 }
