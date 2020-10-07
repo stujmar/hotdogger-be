ed15725575144349b0cdfb56a646122f2b57c60b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-06 20:45:43 -0500

    added a bunch of annotations to the apprunner

--- src/main/java/io/hotdogger/login/LoginApplication.java
+++ src/main/java/io/hotdogger/login/LoginApplication.java
@@ -3,12 +3,18 @@ package io.hotdogger.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
+@ComponentScan({"io.hotdogger.login.players"})
+@EntityScan("io.hotdogger.login.players")
+@EnableJpaRepositories("io.hotdogger.login.players")
 public class LoginApplication {
 
 	public static void main(String[] args) {
