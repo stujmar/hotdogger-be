218bc1085fd90b9d8c980a0c7fb10445a7865718
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-05 20:15:21 -0600

    made a SaveGame PK and got it running again

--- src/main/java/io/hotdogger/login/savegame/SaveGame.java
+++ src/main/java/io/hotdogger/login/savegame/SaveGame.java
@@ -1,9 +1,12 @@
 package io.hotdogger.login.savegame;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "players") 
+@Table(name = "players")
 public class SaveGame {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
