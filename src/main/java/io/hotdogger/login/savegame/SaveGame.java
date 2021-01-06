ff01c4757727021f0908dd3721ef0fff15611d62
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-05 20:18:29 -0600

    added dogCount and constructors

--- src/main/java/io/hotdogger/login/savegame/SaveGame.java
+++ src/main/java/io/hotdogger/login/savegame/SaveGame.java
@@ -1,12 +1,27 @@
 package io.hotdogger.login.savegame;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import static io.hotdogger.login.constants.StringConstants.REQUIRED_FIELD;
 
 @Entity
-@Table(name = "players")
+@Table(name = "saves")
 public class SaveGame {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Dog Count" + REQUIRED_FIELD)
+    private Integer dogCount;
+
+    public SaveGame() {
+
+    }
+
+    public SaveGame(Long id, @NotBlank(message = "Dog Count" + REQUIRED_FIELD) Integer dogCount) {
+        this.id = id;
+        this.dogCount = dogCount;
+    }
 }
