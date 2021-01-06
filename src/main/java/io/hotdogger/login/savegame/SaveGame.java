1e017e1cfef9f9b35b2bb930bd476d146090b0fc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-05 20:24:49 -0600

    getters setters and toString added to SaveGame

--- src/main/java/io/hotdogger/login/savegame/SaveGame.java
+++ src/main/java/io/hotdogger/login/savegame/SaveGame.java
@@ -17,11 +17,34 @@ public class SaveGame {
     private Integer dogCount;
 
     public SaveGame() {
-
     }
 
     public SaveGame(Long id, @NotBlank(message = "Dog Count" + REQUIRED_FIELD) Integer dogCount) {
         this.id = id;
         this.dogCount = dogCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDogCount() {
+        return dogCount;
+    }
+
+    public void setDogCount(Integer dogCount) {
+        this.dogCount = dogCount;
+    }
+
+    @Override
+    public String toString() {
+        return "SaveGame{" +
+                "id=" + id +
+                ", dogCount=" + dogCount +
+                '}';
     }
 }
