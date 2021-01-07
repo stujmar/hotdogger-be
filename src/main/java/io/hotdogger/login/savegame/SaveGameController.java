2fa6ff0bd5b82101ef6547b94a3fbc856b0fb9a9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-06 19:34:29 -0600

    autowired Service into 'saves' controller

--- src/main/java/io/hotdogger/login/savegame/SaveGameController.java
+++ src/main/java/io/hotdogger/login/savegame/SaveGameController.java
@@ -1,9 +1,14 @@
 package io.hotdogger.login.savegame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/saves")
 public class SaveGameController {
+
+    @Autowired
+    private SaveGameService saveGameService;
+
 }
